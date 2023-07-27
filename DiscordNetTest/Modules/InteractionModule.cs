f0c02c989dba73b780c4f776b89a773501c5ae4f
Dmitry <dimonbyt@gmail.com>
2023-07-27 04:15:40 +0300

    add InteractionHandler.cs         InteractionModule.cs         PrefixModule.cs

---
+++ DiscordNetTest/Modules/InteractionModule.cs
@@ -0,0 +1,15 @@
+﻿using Discord.Interactions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordNetTest.Modules
+{
+    public class InteractionModule : InteractionModuleBase<Sock>
+    {
+
+    }
+}
